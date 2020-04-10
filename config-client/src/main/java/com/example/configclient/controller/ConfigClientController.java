43a842df1998b897456cff151c5c1daf309fc330
rui tang <tassassin@sina.com>
2020-04-10 17:13:10 +0800

    config module

---
+++ config-client/src/main/java/com/example/configclient/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.example.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tr
+ * @date 2020/4/10 16:11
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
